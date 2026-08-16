368670ab2206e46e9aa6aa6a1db3cce079e3e9f7
pedroviniciusbar <pedrovinicius36@gmail.com>
2026-08-16 14:11:03 -0300

    Update Elegoo Neptune 2 configuration: change driver types to TMC2209, adjust current settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -167,7 +167,7 @@
 //#define U_DRIVER_TYPE  A4988
 //#define V_DRIVER_TYPE  A4988
 //#define W_DRIVER_TYPE  A4988
-#define E0_DRIVER_TYPE TMC2209_STANDALONE
+#define E0_DRIVER_TYPE A4988  // Extrusor: A4988 (VREF configurado via potenciometro)
 //#define E1_DRIVER_TYPE A4988
 //#define E2_DRIVER_TYPE A4988
 //#define E3_DRIVER_TYPE A4988
@@ -1885,7 +1885,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
